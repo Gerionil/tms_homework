3e618e9798f97e2c41af8a413f1532ee7ecaa47c
Gerionil <67752928+Gerionil@users.noreply.github.com>
2021-07-05 14:15:47 +0300

    remove repeat_js_2

--- repeat_js_2/fibonacci.js
+++
@@ -1,9 +0,0 @@
-//- Посчитать первые 8 чисел Фибоначчи использую рекурсию. Например: fibonachi(n); (При вызове функции положите n = 8);
-
-
-function fibonacci(n) {
-    return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
-  }
-  
-
-  fibonacci(8);
