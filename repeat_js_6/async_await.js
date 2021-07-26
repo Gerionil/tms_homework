0ad445a5b3bb98b9f73b4713b6e085a792dd3d0a
Gerionil <67752928+Gerionil@users.noreply.github.com>
2021-07-26 09:28:58 +0300

    modified

--- repeat_js_6/async_await.js
+++ repeat_js_6/async_await.js
@@ -1,13 +1,4 @@
-function cleanRoom(dirtyLevel = 0) {
-  return new Promise(function (resolve, reject) {
-    if (dirtyLevel >= 0 && dirtyLevel <= 4) {
-      setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
-    } else if (dirtyLevel > 4) {
-      setTimeout(() => reject(dirtyLevel), dirtyLevel * 1000);
-    }
-  });
-}
-
+//последовательная уборка трех комнат
 async function cleanRoom(dirtyLevel = 0) {
   let promise = new Promise(function (resolve, reject) {
     if (dirtyLevel >= 0 && dirtyLevel <= 4) {
@@ -17,56 +8,54 @@ async function cleanRoom(dirtyLevel = 0) {
     }
   });
   let result = await promise;
-  console.log(result);
 }
 
 cleanRoom(2);
 
 async function cleanRooms(dirtyLevel_1 = 0, dirtyLevel_2 = 0, dirtyLevel_3 = 0){
+    try{
     let result1 = await cleanRoom(dirtyLevel_1);
     let result2 = await cleanRoom(dirtyLevel_2);
     let result3 = await cleanRoom(dirtyLevel_3);
     console.log('Уборка завершена')
-}
-
-cleanRooms(1, 2, 3);
-
-function cleanRooms(dirtyLevel_1 = 0, dirtyLevel_2 = 0, dirtyLevel_3 = 0) {
-  cleanRoom(dirtyLevel_1)
-    .then((result) => {
-      console.log(`Первая комната убрана за ${result}`);
-      return cleanRoom(dirtyLevel_2);
-    })
-    .then((result) => {
-      console.log(`Вторая комната убрана за ${result}`);
-      return cleanRoom(dirtyLevel_3);
-    })
-    .then((result) =>
-      console.log(`Третья комната убрана за ${result}. Уборка завершена!`)
-    )
-    .catch((error) => console.log("Невозможно. Уровень загрязнения", error));
-}
-
-
-
-function cleanRoom2(dirtyLevel = 0) {
-  return new Promise(function (resolve, reject) {
-    if (dirtyLevel >= 0 && dirtyLevel <= 4) {
-      setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
-    } else if (dirtyLevel > 4) {
-      setTimeout(() => reject(dirtyLevel), dirtyLevel * 1000);
+    }catch (error){
+        console.log('НЕВОЗМОЖНО ЭТО УБРАТЬ')
     }
-  });
+    
 }
 
-function cleanRooms2(dirtyLevel_1 = 0, dirtyLevel_2 = 0, dirtyLevel_3 = 0) {
-  Promise.all([
+cleanRooms(1, 2, 3)
+
+
+
+//параллельная уборка трех комнат
+
+
+async function cleanRoom2(dirtyLevel = 0) {
+    let promise = new Promise(function (resolve, reject) {
+        if (dirtyLevel >= 0 && dirtyLevel <= 4) {
+          setTimeout(() => resolve(`Комната убрана за время ${dirtyLevel}`), dirtyLevel * 1000);
+        } else if (dirtyLevel > 4) {
+          setTimeout(() => reject("Ошибка"), dirtyLevel * 1000);
+        }
+      });
+      let result = await promise;
+}
+
+
+async function cleanRooms2(dirtyLevel_1 = 0, dirtyLevel_2 = 0, dirtyLevel_3 = 0) {
+  try{
+    let result = await Promise.all([
     cleanRoom2(dirtyLevel_1),
     cleanRoom2(dirtyLevel_2),
     cleanRoom2(dirtyLevel_3),
-  ]).then(
-    (result) => console.log("Уборка выполнена"),
-    (err) => console.log("Невозможно")
-  );
+  ])
+
+    console.log("Уборка выполнена")
+} catch(error){
+    console.log("Невозможно")
+}
+    
+  
 }
 cleanRooms2(1, 2, 3);
